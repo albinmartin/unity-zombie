2cdce8d39be6f729cbed73cf134a231aa4452227
albinmartin <albwe681@gmail.com>
2014-02-14 16:48:32 +0100

    added kinectwrapper and keyboard movement

---
+++ Assets/keyboardMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class keyboardMovement : MonoBehaviour {
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.1f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 curPos = gameObject.transform.position;
+
+		if(Input.GetKey(KeyCode.D)){
+			gameObject.transform.Translate(Vector3.right*speed);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			gameObject.transform.Translate(Vector3.left*speed);
+		}
+		if(Input.GetKey(KeyCode.W)){
+			gameObject.transform.Translate(Vector3.up*speed);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			gameObject.transform.Translate(Vector3.down*speed);
+		}
+	}
+}
